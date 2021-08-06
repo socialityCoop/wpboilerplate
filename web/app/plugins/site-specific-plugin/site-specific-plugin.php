d5fb1130ee885e00586b9d5c1853dc553c850069
Gregory Tsardanidis <pastsar@gmail.com>
2021-08-06 14:50:33 +0300

    Update site-specific-plugin.php

---
+++ web/app/plugins/site-specific-plugin/site-specific-plugin.php
@@ -0,0 +1,5 @@
+<?php
+/*
+Plugin Name: Site Specific Plugin
+Description: This is a Site Specific Plugin for newproject.local. Thus its Code changes go for the site regardless of the Site Active Theme.
+*/
